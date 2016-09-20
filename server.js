4e8c9a5d83c3a993d6dd8759333a1a2f41c076d8
madhanvv21 <madhanvv21@gmail.com>
2016-09-20 09:11:24 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/article1',function(req,res){
-    res.send("This is article one");
+    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/ui/article2',function(req,res){
-    res.send("This is article two");
+    res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 app.get('/ui/article3',function(req,res){
-    res.send("This is article three");
+    res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
