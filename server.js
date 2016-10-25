bbfcbeb6608f3869a47b566b3b7a84e8a0651ce3
Madhan <madhanvv21@gmail.com>
2016-10-25 07:57:39 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,8 +89,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var pool = new Pool(config);
 app.get('/text-db', function (req, res){
+    pool.query('SELECT * FROM test', function(req,res){
+        if(err){
+            res.status(500).send(err.toString());
+            }else{
+                res.send(JSON.stringify(result));
+            }
+    });
     
 });
 
